539fbee9a7c00f07861e9cc5d757593e712f8c91
Zachary Klein <zak@silver-chalice.com>
2020-04-01 09:50:51 -0500

    Added Swagger docs to Gateway

--- gateway/src/main/java/com/objectcomputing/Application.java
+++ gateway/src/main/java/com/objectcomputing/Application.java
@@ -1,7 +1,20 @@
 package com.objectcomputing;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Gateway",
+                version = "0.0",
+                description = "Micronaut SPA Demo",
+                license = @License(name = "MIT", url = "https://opensource.org/licenses/MIT"),
+                contact = @Contact(url = "http://objectcomputing.com.com", name = "Zachary Klein", email = "kleinz@objectcomputing.com")
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
