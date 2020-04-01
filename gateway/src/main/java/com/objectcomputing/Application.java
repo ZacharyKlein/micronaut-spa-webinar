b454627ef731287e7100b94c6fb34efd7f451c17
Zachary Klein <zak@silver-chalice.com>
2020-04-01 13:30:12 -0500

    Corrected a typo in contact URL

--- gateway/src/main/java/com/objectcomputing/Application.java
+++ gateway/src/main/java/com/objectcomputing/Application.java
@@ -12,7 +12,7 @@ import io.swagger.v3.oas.annotations.info.License;
                 version = "0.0",
                 description = "Micronaut SPA Demo",
                 license = @License(name = "MIT", url = "https://opensource.org/licenses/MIT"),
-                contact = @Contact(url = "http://objectcomputing.com.com", name = "Zachary Klein", email = "kleinz@objectcomputing.com")
+                contact = @Contact(url = "http://objectcomputing.com", name = "Zachary Klein", email = "kleinz@objectcomputing.com")
         )
 )
 public class Application {
